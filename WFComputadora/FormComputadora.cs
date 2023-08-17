0428826d6851734c089250ff021cc4b64d1729f1
Gaston Ibañez <gastonibanezcv@gmail.com>
2023-08-16 21:02:34 -0300

    Comentarios de Form Comptadora.

--- WFComputadora/FormComputadora.cs
+++ WFComputadora/FormComputadora.cs
@@ -26,6 +26,7 @@ namespace WFComputadora
 
         private void LLenarDGV()
         {
+            // Limpia y llena el DataGridView con datos de la base de datos.
             Dgv_computadora.Rows.Clear();
             DataSet ds = new DataSet();
             ds = objCapaComputadora.ListadoComputadoras("Todos");
@@ -44,6 +45,7 @@ namespace WFComputadora
 
         private void Dgv_iniciar()
         {
+            // Da inicio a las columnas del DataGridView.
             Dgv_computadora.Columns.Add("0", "Codigo");
             Dgv_computadora.Columns.Add("1", "Modelo");
             Dgv_computadora.Columns.Add("2", "Aplicaciones");
@@ -54,82 +56,95 @@ namespace WFComputadora
 
         private bool ValidarFormatoCuil(string cuil)
         {
-            string patron = @"\d{2}-\d{8}-\d{1}";
-            return System.Text.RegularExpressions.Regex.IsMatch(cuil, patron);
+            // Verifica si el CUIL tiene el formato correcto (XX-XXXXXXXX-X).
+            string pat = @"\d{2}-\d{8}-\d{1}";
+            return System.Text.RegularExpressions.Regex.IsMatch(cuil, pat);
         }
         private bool ValidarFormatoPatente(string patente)
         {
-            // El patrón de expresión regular para el formato XXX999
-            string patron = @"^[A-Z]{3}\d{3}$";
+            // Verifica si la patente tiene el formato correcto (ASD123).
+            string pat = @"^[A-Z]{3}\d{3}$";
 
-            // Verificar si la patente coincide con el patrón
-            return System.Text.RegularExpressions.Regex.IsMatch(patente, patron);
+            // Verificia si la patente coincide con el patrón.
+            return System.Text.RegularExpressions.Regex.IsMatch(patente, pat);
         }
         private bool ValidarCodigoUnico(int codigo)
         {
+            // Recorre todas las filas del DataGridView.
             foreach (DataGridViewRow row in Dgv_computadora.Rows)
             {
+                // Verifica que la columna del código no sea nulla y que coincida con el código que esta validando.
+               
                 if (row.Cells[0].Value != null && row.Cells[0].Value.ToString() == codigo.ToString())
                 {
-                    return false; // El código ya existe en una fila
+                    return false; // El código ya existe en una fila.
                 }
             }
-            return true; // El código es único
+            return true; // El código es único.
         }
 
         private bool ValidarCuilUnico(string cuil)
         {
+            // Recorre todas las filas del DataGridView.
             foreach (DataGridViewRow row in Dgv_computadora.Rows)
             {
+                // Verifica que la columna del Cuil no sea nulo y que coincida con el cuil que esta validando.
                 if (row.Cells[4].Value != null && row.Cells[4].Value.ToString() == cuil)
                 {
-                    return false; // El CUIL ya existe en una fila
+                    return false; // El CUIL ya existe en una fila.
                 }
             }
-            return true; // El CUIL es único
+            return true; // El CUIL es único.
         }
 
         private bool ValidarPatenteUnica(string patente)
         {
+
+            // Verifica que la columna de la patente no sea nula y que coincida con la patente que esta validando.
             foreach (DataGridViewRow row in Dgv_computadora.Rows)
             {
                 if (row.Cells[5].Value != null && row.Cells[5].Value.ToString() == patente)
                 {
-                    return false; // La patente ya existe en una fila
+                    return false; // La patente ya existe en una fila.
                 }
             }
-            return true; // La patente es única
+            return true; // La patente es única.
         }
 
         private void Btn_cargar_Click(object sender, EventArgs e)
         {
+            // Obtener los valores de CUIL y patente desde los TextBox.
             string cuil = Txtb_cuil.Text;
             string patente = Txtb_patente.Text;
 
+
+            // Valida que los campos obligatorios no estén vacíos.
             if (string.IsNullOrEmpty(Txtb_codigo.Text) || string.IsNullOrEmpty(Txtb_modelo.Text))
             {
                 MessageBox.Show("Debe completar todos los campos obligatorios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
+            // Valida que el código sea un número válido.
             if (!int.TryParse(Txtb_codigo.Text, out int codigo))
             {
                 MessageBox.Show("Ingrese un valor válido para el código, solo debe contener números.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            // Valida que las aplicaciones sean un número válido.
 
             if (!int.TryParse(Txtb_aplicaciones.Text, out int aplicaciones))
             {
                 MessageBox.Show("Ingrese un valor válido para las aplicaciones, solo debe contener números.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
+            // Valida que se ingrese un valor para el CUIL
             if (string.IsNullOrEmpty(Txtb_cuil.Text))
             {
                 MessageBox.Show("Debe ingresar un valor para el CUIL.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
+            // Valida que la cantidad de aplicaciones esté en el rango permitido, (Hasta 15 aplicaciones).
             if (!int.TryParse(Txtb_aplicaciones.Text, out int cantidadAplicaciones) || cantidadAplicaciones < 0 || cantidadAplicaciones > 15)
             {
                 if (cantidadAplicaciones > 15)
@@ -142,41 +157,47 @@ namespace WFComputadora
                 }
                 return;
             }
+            // Valida que se ingrese una patente.
+
             if (string.IsNullOrEmpty(Txtb_patente.Text))
             {
                 MessageBox.Show("Debe ingresar una patente.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
+            // Valida que se ingrese correcamente el formato de la patente, (ASD123).
             if (!ValidarFormatoPatente(Txtb_patente.Text))
             {
                 MessageBox.Show("Ingrese una patente válida en el formato ASD123.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
+            // Valida que se ingrese correcamente el formato del Cuil, (11-12345678-1).
             if (!ValidarFormatoCuil(Txtb_cuil.Text))
             {
                 MessageBox.Show("Ingrese un CUIL válido en el formato 11-12345678-1.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            // Valida que el Código sea único.
             if (!ValidarCodigoUnico(codigo))
             {
                 MessageBox.Show("El código ingresado ya existe. Ingrese un código único.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
+            // Valida que el Cuil sea único.
             if (!ValidarCuilUnico(cuil))
             {
                 MessageBox.Show("El CUIL ingresado ya existe. Ingrese un CUIL único.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
+            // Valida que la patente sea única.
             if (!ValidarPatenteUnica(patente))
             {
                 MessageBox.Show("La patente ingresada ya existe. Ingrese una patente única.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
+
+            // Crea una instancia de la clase Computadora y carga los datos ingresados en los TextBox.
             Computadora NuevaCompu;
             NuevaCompu = new Computadora(
                 codigo,
@@ -186,8 +207,11 @@ namespace WFComputadora
                 patente
             );
 
-            DateTime fechaSeleccionada = Datetime_actualizacion.Value;
-            NuevaCompu.ActualizarUltimaActualizacion(fechaSeleccionada);
+
+            DateTime fechaSeleccionada = Datetime_actualizacion.Value; // Obtener la fecha seleccionada en el DateTimePicker
+            NuevaCompu.ActualizarUltimaActualizacion(fechaSeleccionada); // Actualizar la última fecha de actualización de la computadora
+
+            // Muestra los datos cargados en el Datagridview en los Label del TabMovimiento.
 
             Lbl_codigomov.Text = "Código: " + NuevaCompu.Pcodigo.ToString();
             Lbl_modelomov.Text = "Modelo: " + NuevaCompu.Pmodelo;
@@ -196,32 +220,34 @@ namespace WFComputadora
             Lbl_aplicacionesmov.Text = "Hay " + NuevaCompu.Paplicaciones.ToString() + " aplicaciones ejecutándose en este momento";
             Lbl_patentemov.Text = "Patente: " + NuevaCompu.Ppatente;
 
+            // Cambia a la pestaña de Tab Moviemiento.
             Tab_computadorac.SelectedTab = Tab_movimientoc;
 
+            // Muestra un modal si todos los datos estan cargados correctamente.
             MessageBox.Show("Datos de la computadora cargados", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            Dgv_computadora.Rows.Add(
-                NuevaCompu.Pcodigo.ToString(),
-                NuevaCompu.Pmodelo,
-                NuevaCompu.Paplicaciones.ToString(),
-                NuevaCompu.PultimaActualizacion.ToString("dd/MM/yyyy HH:mm:ss"),
-                NuevaCompu.Pcuil,
-                NuevaCompu.Ppatente
-            );
-
+            // Agrega los datos de la computadora al DataGridView.
+            Dgv_computadora.Rows.Add(NuevaCompu.Pcodigo, NuevaCompu.Pmodelo, NuevaCompu.Paplicaciones, NuevaCompu.PultimaActualizacion.ToString("dd/MM/yyyy HH:mm:ss"), NuevaCompu.Pcuil, NuevaCompu.Ppatente);
+            // Actualiza la variable de fila.
             Fila = Dgv_computadora.Rows.Count - 1;
         }
 
+
+        // Maneja el botón "Salir" del formulario de la pestaña Select Tab.
         private void Btn_salir_Click(object sender, EventArgs e)
         {
+            // Muestra un modal de confirmación de salida.
             DialogResult salir = MessageBox.Show("¿Está seguro que desea salir?", "Confirme si desea salir.", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
+            // Si el usuario confirma la salida
             if (salir == DialogResult.Yes)
             {
+                // Cierra la aplicación
                 Application.Exit();
             }
         }
 
+        // Maneja el botón "Salir" del formulario de la pestaña del TabMovimiento.
         private void Btn_salir2_Click(object sender, EventArgs e)
         {
             DialogResult salir = MessageBox.Show("¿Está seguro que desea salir?", "Confirme si desea salir.", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
@@ -232,37 +258,41 @@ namespace WFComputadora
             }
         }
 
+
+        // Maneja el botón "Borrar" del formulario
         private void Btn_borrar_Click(object sender, EventArgs e)
         {
-            // Verificar si hay al menos una fila en el DataGridView
+            // Verifica si hay al menos una fila en el DataGridView
             if (Dgv_computadora.Rows.Count > 0)
             {
-                // Verificar si se ha seleccionado una fila completa
+                // Verifica si se ha seleccionado una fila completa
                 if (Dgv_computadora.SelectedRows.Count > 0)
                 {
-                    // Mostrar un MessageBox de confirmación
+                    // Muestra un modal de confirmación.
                     DialogResult resultado = MessageBox.Show("¿Está seguro que desea eliminar la fila seleccionada?", "Confirmar eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
                     if (resultado == DialogResult.Yes)
                     {
-                        // Obtener la fila seleccionada
+                        // Obtiene una fila seleccionada.
                         DataGridViewRow filaSeleccionada = Dgv_computadora.SelectedRows[0];
 
-                        // Verificar si la fila no está vacía
+                        // Verifica si la fila no está vacía.
                         if (filaSeleccionada != null && !filaSeleccionada.IsNewRow)
                         {
-                            // Eliminar la fila seleccionada
+                            // Elimina la fila seleccionada.
                             Dgv_computadora.Rows.Remove(filaSeleccionada);
                         }
                     }
                 }
                 else
-                {
+                { 
+                    // Modal de error si no se selecciono una fila para borrar
                     MessageBox.Show("No se ha seleccionado una fila para borrar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
             else
             {
+                // Modal de error si no hay filas para borrar.
                 MessageBox.Show("No hay filas para borrar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
